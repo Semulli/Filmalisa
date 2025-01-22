a391eedd15ed542eb459a3e8c3c50da144161e42
Semulli <semabdulazizova@gmail.com>
2025-01-22 23:44:03 +0400

    solve problem

--- Scripts/Client/favorite.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  const mainToken = sessionStorage.getItem("access_token");
-
-  if (!mainToken) {
-    window.location.href = "../../Pages/Client/login.html";
-  }
-});
